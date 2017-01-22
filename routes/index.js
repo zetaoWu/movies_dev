3bafcaf84712865a33c244424e7f918a9719fc3a
zetaoWu <506495182@qq.com>
2017-01-22 16:06:08 +0800

    最终版 无bug

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ var Category =require('../app/controllers/category');
 var express = require('express');
 var router = express.Router();
 
+var multiparty=require('connect-multiparty');
+var multipartyMiddleWare=multiparty();
+
 //预处理 。pre handle user
 router.use(function (req, res, next) {
     var _user = req.session.user;
@@ -27,7 +30,7 @@ router.get('/movie/:id', Movie.detail);
 router.get('/admin/movie/update/:id',User.signinRequired,User.adminRequired,  Movie.update);
 router.get('/admin/movie/new', User.signinRequired,User.adminRequired, Movie.new);
 router.get('/admin/movie/list', User.signinRequired,User.adminRequired, Movie.list)
-router.post('/admin/movie',User.signinRequired,User.adminRequired,Movie.save);
+router.post('/admin/movie',multipartyMiddleWare,User.signinRequired,User.adminRequired,Movie.savePoster,Movie.save);
 router.delete('/admin/movie/list',User.signinRequired,User.adminRequired, Movie.del);
 
 //user
@@ -40,5 +43,7 @@ router.get('/admin/user/list', User.signinRequired,User.adminRequired,User.list)
 
 //comment
 router.post('/user/comment',User.signinRequired,Comment.save);
+
+router.get('/results',Index.search);
 
 module.exports = router;
